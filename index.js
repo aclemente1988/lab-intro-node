3ae8c3d4ad1c5cca44319dfc5ad9c5f01f99fc6c
Axel <axel_clemente@yahoo.com>
2022-10-27 17:38:23 +0200

    done

--- index.js
+++ index.js
@@ -1,13 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+ 
 
-  get(pos) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+    } 
+   
+  
 
-  max() {}
+  get(pos) {
+    if (pos < this.length) {
+      return this.items[pos];
+    }
+    throw new Error('OutOfBounds');
+  }
 
-  min() {}
+  max() {
+    if (this.items.length > 0) {
+      let highest = 0;
+      for (let i = 0; i < this.items.length; i++) {
+        if (highest < this.items[i]) {
+          highest = this.items[i];
+        }
+      }
+      return highest;
+    }
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+  }
+
+  min() {
+    if (this.items.length == 0) {
+      throw new Error("EmptySortedList");
+    }
+    if (this.items.length >0){
+      return this.items[0];
+    }
+   
+  
+  }
+
 
   sum() {}
 
